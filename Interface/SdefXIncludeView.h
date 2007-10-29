a12fcf631491161640e35933ce85cb0ca6d67e09
Jean-Daniel Dupas <jddupas@shadowlab.org>
2007-10-29 18:03:38 +0000

    Path xinclude 2

---
+++ Interface/SdefXIncludeView.h
@@ -0,0 +1,15 @@
+//
+//  SdefXIncludeView.h
+//  Sdef Editor
+//
+//  Created by Jean-Daniel Dupas on 29/10/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "SdefViewController.h"
+
+@interface SdefXIncludeView : SdefViewController {
+  @private
+}
+
+@end
---
+++ Interface/SdefXIncludeView.m
@@ -0,0 +1,13 @@
+//
+//  SdefXIncludeView.m
+//  Sdef Editor
+//
+//  Created by Jean-Daniel Dupas on 29/10/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "SdefXIncludeView.h"
+
+@implementation SdefXIncludeView
+
+@end
